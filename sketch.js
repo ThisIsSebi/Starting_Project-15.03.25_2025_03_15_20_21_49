a3800f676f4aed25f7d5a5fc48ba1b1f9e54d163
ThisIsSebi <shoff439@gmail.com>
2025-03-15 22:22:40 +0100

    added another circle

--- sketch.js
+++ sketch.js
@@ -4,4 +4,11 @@ function setup() {
 
 function draw() {
   background(0, 0 ,200);
+  stroke("red")
+  strokeWeight(2)
+  line(0,0,400, 400)
+  line(400, 0, 0, 400)
+  rect(50, 150, 100, 100, 100);
+  rect(250, 150, 100, 100, 100);
+  describe("My first project using p5.js")
 }
