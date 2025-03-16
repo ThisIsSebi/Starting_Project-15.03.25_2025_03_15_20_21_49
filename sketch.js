0e8b967d5dea242a4aa4b89fef7008c9f7f0f338
ThisIsSebi <shoff439@gmail.com>
2025-03-16 20:07:31 +0100

    added different colours

--- sketch.js
+++ sketch.js
@@ -3,12 +3,23 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0 ,200);
+  background(200, 200 ,200);
   stroke("red")
   strokeWeight(2)
   line(0,0,400, 400)
   line(400, 0, 0, 400)
+  fill(0,0,4)
+  stroke(0,255,0)
+  strokeWeight(5)
   rect(50, 150, 100, 100, 100);
+  fill(0, 0, 0)
+  stroke(0,255,0)
+  strokeWeight(4)
   rect(250, 150, 100, 100, 100);
+  fill(0,0,0,50)
+  strokeWeight(4)
+  fill(0,0,0)
+  ellipse(200,50,100,75)
+  rect(150,300,100,75)
   describe("My first project using p5.js")
 }
